6ba1cc49ad2211fcde56f1b1fc70683b31925636
Chris Alberti <christopherjalberti@gmail.com>
2018-04-21 20:55:57 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   languages.each do |style, names|
     names.each do |name, type|
       if !new_hash.has_key?(name)
-        new_hash[name] = {:type => type, :style => style}
+        new_hash[name] = {:type => type, :style => [style]}
       else
         new_hash[name][:style] << style
       end
